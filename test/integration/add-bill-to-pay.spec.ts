e8b1c5a0eb73d553bb458295108554d19de2039c
Abner Ferrari <aeliferrari@gmail.com>
2023-11-01 02:13:33 -0300

    ajuste

--- test/integration/add-bill-to-pay.spec.ts
+++ test/integration/add-bill-to-pay.spec.ts
@@ -1,6 +1,5 @@
 import { describe, expect, it } from "vitest"
 import makeBillToPay from "../factories/make-bill-to-pay"
-import makeAddBillToPay from "../factories/make-add-bill-to-pay"
 import AddBillToPay from "@/application/usecase/add-bill-to-pay"
 import BillToPayRepository from "@/application/usecase/repository/bill-to-pay-repository"
 import { BillToPay } from "@/domain/bill-to-pay/bill-to-pay"
